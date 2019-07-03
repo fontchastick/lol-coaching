5e34c9359c0b5ba71a2ba443584adf5bebe5c398
JB <toubibharmony@yahoo.com>
2019-07-03 16:11:50 +0200

    heroku solution best 2

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/lol-coach'));
+app.use(express.static(__dirname + '/dist/lol-coach'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname,'/dist/lol-coach/index.html'));
+res.sendFile(path.join(__dirname+'/dist/lol-coach/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 // // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/<name-of-app>'));
+// app.use(express.static(__dirname + '/dist/lol-coach'));
 
 // app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+// res.sendFile(path.join(__dirname+'/dist/lol-coach/index.html'));
 // });
 
 // Start the app by listening on the default Heroku port
